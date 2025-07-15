9893b1508585efa41935d2b1ebbe34d7cbcff6dc
SirBeans <ryanchung0084@gmail.com>
2025-07-15 19:14:55 -0400

    whatever

--- src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
@@ -4,12 +4,12 @@ package com.team4099.robot2025
  * Automatically generated file containing build version information.
  */
 const val MAVEN_GROUP = ""
-const val MAVEN_NAME = "Offseason-2025-Code-Clone"
+const val MAVEN_NAME = "Offseason-2025-P3REGRINE"
 const val VERSION = "unspecified"
-const val GIT_REVISION = -1
-const val GIT_SHA = "UNKNOWN"
-const val GIT_DATE = "UNKNOWN"
-const val GIT_BRANCH = "UNKNOWN"
-const val BUILD_DATE = "2025-06-01T16:41:44Z"
-const val BUILD_UNIX_TIME = 1748814104994L
-const val DIRTY = 129
+const val GIT_REVISION = 1
+const val GIT_SHA = "0aa43faed68f862648d788c1a8f981f905cb1e36"
+const val GIT_DATE = "2025-06-01T17:10:26Z"
+const val GIT_BRANCH = "main"
+const val BUILD_DATE = "2025-07-07T18:21:02Z"
+const val BUILD_UNIX_TIME = 1751930462513L
+const val DIRTY = 1
---
+++ src/main/kotlin/com/team4099/robot2025/config/constants/ArmConstants.kt
@@ -0,0 +1,8 @@
+package com.team4099.robot2025.config.constants
+
+import org.team4099.lib.units.derived.volts
+
+object ArmConstants {
+    val VOLTAGE_COMPENSATION = 12.0.volts
+    val ENCODER_TO_MECHANISM_GEAR_RATIO = 1/2
+}

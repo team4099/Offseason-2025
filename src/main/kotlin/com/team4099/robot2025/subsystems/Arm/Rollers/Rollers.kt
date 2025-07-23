c5306b9c53d5848008e8a35908689b5638c87665
SirBeans <146898838+SirBeans@users.noreply.github.com>
2025-07-22 21:51:27 -0400

    Update Rollers.kt

--- src/main/kotlin/com/team4099/robot2025/subsystems/Arm/Rollers/Rollers.kt
+++ src/main/kotlin/com/team4099/robot2025/subsystems/Arm/Rollers/Rollers.kt
@@ -43,7 +43,7 @@ class Rollers(val io: RollersIO):SubsytemBase() {
       field = value
     }
 
-  override periodic() {
+  override fun periodic() {
     io.updateInputs(inputs)
     CustomLogger.processInputs("Rollers", inputs)
     CustomLogger.recordOutput("Rollers/currentState", currentState.toString())

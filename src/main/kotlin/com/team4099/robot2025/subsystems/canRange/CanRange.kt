60548232d0cbf85c30267433b54366db44e93152
SirBeans <ryanchung0084@gmail.com>
2025-07-29 21:15:48 -0400

    add some variables for nathan to call

--- src/main/kotlin/com/team4099/robot2025/subsystems/canRange/CanRange.kt
+++ src/main/kotlin/com/team4099/robot2025/subsystems/canRange/CanRange.kt
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 class CanRange(val io: CANRangeIO): SubsystemBase() {
     val inputs = CANRangeIO.CANRangeIOInputs()
+    var distance = inputs.distance
+    var isDetected = inputs.isDetected
     override fun periodic(){
         io.updateInputs(inputs)
     }

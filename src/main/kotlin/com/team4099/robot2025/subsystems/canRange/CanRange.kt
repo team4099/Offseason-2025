c31bb45fc9186251f73fbb08904b8d41c031ec35
SirBeans <ryanchung0084@gmail.com>
2025-07-29 21:18:34 -0400

    spotless

--- src/main/kotlin/com/team4099/robot2025/subsystems/canRange/CanRange.kt
+++ src/main/kotlin/com/team4099/robot2025/subsystems/canRange/CanRange.kt
@@ -2,11 +2,11 @@ package com.team4099.robot2025.subsystems.canRange
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
-class CanRange(val io: CANRangeIO): SubsystemBase() {
-    val inputs = CANRangeIO.CANRangeIOInputs()
-    var distance = inputs.distance
-    var isDetected = inputs.isDetected
-    override fun periodic(){
-        io.updateInputs(inputs)
-    }
+class CanRange(val io: CANRangeIO) : SubsystemBase() {
+  val inputs = CANRangeIO.CANRangeIOInputs()
+  var distance = inputs.distance
+  var isDetected = inputs.isDetected
+  override fun periodic() {
+    io.updateInputs(inputs)
+  }
 }

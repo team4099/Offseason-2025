8a038660cadf7683f3e4558b03e9f8003f45da62
yamamara <yamamrtyu@gmail.com>
2025-07-29 22:47:23 -0400

    Add null checks to IO interface

--- src/main/kotlin/com/team4099/robot2025/subsystems/canRange/CANRangeIO.kt
+++ src/main/kotlin/com/team4099/robot2025/subsystems/canRange/CANRangeIO.kt
@@ -13,19 +13,21 @@ interface CANRangeIO {
     var isDetected = false
     var supplyVoltage = 0.0.volts
     var ambientSignal = 0.0
-    override fun toLog(table: LogTable) {
-      table.put("distance", distance.inMeters)
-      table.put("isDetected", isDetected)
-      table.put("supplyVoltage", supplyVoltage.inVolts)
-      table.put("ambientSignal", ambientSignal)
+
+    override fun toLog(table: LogTable?) {
+      table?.put("distance", distance.inMeters)
+      table?.put("isDetected", isDetected)
+      table?.put("supplyVoltage", supplyVoltage.inVolts)
+      table?.put("ambientSignal", ambientSignal)
     }
 
-    override fun fromLog(table: LogTable) {
-      table.get("distance", distance.inMeters).let { distance = it.meters }
-      table.get("supplyVoltage", supplyVoltage.inVolts).let { supplyVoltage = it.volts }
-      table.get("isDetected", isDetected)
-      table.get("ambientSignal", ambientSignal).let { ambientSignal = it }
+    override fun fromLog(table: LogTable?) {
+      table?.get("distance", distance.inMeters)?.let { distance = it.meters }
+      table?.get("supplyVoltage", supplyVoltage.inVolts)?.let { supplyVoltage = it.volts }
+      table?.get("isDetected", isDetected)
+      table?.get("ambientSignal", ambientSignal)?.let { ambientSignal = it }
     }
   }
-  fun updateInputs(inputs: CANRangeIO.CANRangeIOInputs) {}
+
+  fun updateInputs(inputs: CANRangeIOInputs) {}
 }

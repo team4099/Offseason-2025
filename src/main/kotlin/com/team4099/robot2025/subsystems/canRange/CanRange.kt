c2252cfcc3c38f5f1a5770b95314f2dcdaa6f7de
SirBeans <ryanchung0084@gmail.com>
2025-07-29 20:21:55 -0400

    add the class for canrange

---
+++ src/main/kotlin/com/team4099/robot2025/subsystems/canRange/CanRange.kt
@@ -0,0 +1,10 @@
+package com.team4099.robot2025.subsystems.canRange
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+
+class CanRange(val io: CANRangeIO): SubsystemBase() {
+    val inputs = CANRangeIO.CANRangeIOInputs()
+    override fun periodic(){
+        io.updateInputs(inputs)
+    }
+}

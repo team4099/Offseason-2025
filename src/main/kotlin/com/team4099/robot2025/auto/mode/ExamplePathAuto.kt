5a660cc9b012456fcad1833d684525768c19c42b
00magikarp <94652654+00magikarp@users.noreply.github.com>
2025-08-20 13:26:31 -0400

    fix flipping issue

--- src/main/kotlin/com/team4099/robot2025/auto/mode/ExamplePathAuto.kt
+++ src/main/kotlin/com/team4099/robot2025/auto/mode/ExamplePathAuto.kt
@@ -4,8 +4,6 @@ import choreo.Choreo
 import choreo.trajectory.SwerveSample
 import com.team4099.robot2025.commands.drivetrain.FollowChoreoPath
 import com.team4099.robot2025.subsystems.drivetrain.drive.Drivetrain
-import com.team4099.robot2025.util.FMSData
-import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.WaitCommand
 import org.team4099.lib.geometry.Pose2d
@@ -22,7 +20,8 @@ class ExamplePathAuto(val drivetrain: Drivetrain) : SequentialCommandGroup() {
 
   companion object {
     val trajectory = Choreo.loadTrajectory<SwerveSample>("Example/ExamplePath.traj").get()
-    val startingPose =
-      Pose2d(trajectory.getInitialPose(FMSData.allianceColor == DriverStation.Alliance.Red).get())
+
+    // don't flip for red since we reset field frame estimator so it's robot relative
+    val startingPose = Pose2d(trajectory.getInitialPose(false).get())
   }
 }

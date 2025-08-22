f9dfdb62f9c1f04fb82bf4806cee37c63e022708
SirBeans <ryanchung0084@gmail.com>
2025-08-21 20:13:57 -0400

    fix rumble trigger logic

--- src/main/kotlin/com/team4099/robot2025/subsystems/canRange/CANRange.kt
+++ src/main/kotlin/com/team4099/robot2025/subsystems/canRange/CANRange.kt
@@ -3,20 +3,29 @@ package com.team4099.robot2025.subsystems.canRange
 import com.team4099.lib.hal.Clock
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.team4099.lib.units.base.seconds
+import org.team4099.lib.units.base.Time
 
 class CANRange(val io: CANRangeIO) : SubsystemBase() {
   val inputs = CANRangeIO.CANRangeIOInputs()
   var rumbleTrigger = false
-  var rumbleTime = (-1337).seconds
+  private var rumbleStartTime: Time = (-1337).seconds
+  private var alreadyDetected = false
 
   override fun periodic() {
     io.updateInputs(inputs)
 
-    if (inputs.isDetected && (!rumbleTrigger || Clock.fpgaTime - rumbleTime < 0.5.seconds)) {
+    if (inputs.isDetected && !alreadyDetected) {
       rumbleTrigger = true
-      rumbleTime = Clock.fpgaTime
+      rumbleStartTime = Clock.fpgaTime
+      alreadyDetected = true
     } else if (!inputs.isDetected) {
+      rumbleTrigger = false
+      alreadyDetected = false
+    }
+
+    if (rumbleTrigger && (Clock.fpgaTime - rumbleStartTime > 0.5.seconds)) {
       rumbleTrigger = false
     }
   }
 }
+
